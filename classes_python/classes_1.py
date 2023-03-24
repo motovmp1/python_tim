c66306c5ea732d9ebf8ed01f39621aa0d2c43e56
motovmp1 <vinicius.mpinho@gmail.com>
2023-03-24 13:00:01 +0000

    python classes 1 tim rev2

--- classes_python/classes_1.py
+++ classes_python/classes_1.py
@@ -1,15 +1,27 @@
 class Dog(object):
 
-    def __init__(self, name) -> None:
+    def __init__(self, name, age, weight) -> None:
         self.name = name
+        self.age = age
+        self.weight = weight 
         print('Nice you mad a dog class')
 
+
     def speak(self):
-        print("Hi my name is", self.name)
+        print(f"Hi my name is self.name {self.name} and my age is {self.age}" )
+    
+
+    def add_weight(self):
+        print (f"check my weight {self.weight}")
 
 
-tim = Dog('Vini')
-fred = Dog('Sophia')
+
+tim = Dog('Vini', 26, 100)
+fred = Dog('Sophia', 10, 90)
 
 tim.speak()
 fred.speak()
+
+tim.add_weight()
+
+print(tim.weight)
